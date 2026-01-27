e2b871c4bc5cabc4a319009328a29cefcb998883
Mary <mary.sk2000@gmail.com>
2026-01-28 01:40:43 +0300

    feat(products): add product in stock indicator

--- src/components/dummies/ProductCard.tsx
+++ src/components/dummies/ProductCard.tsx
@@ -2,8 +2,9 @@ import type { ProductCardProps } from "@/models/Products/client"
 import { Button, Card, Image, Text } from "@chakra-ui/react"
 
 const ProductCard = ({ product }: ProductCardProps) => {
+   const productInStock = product.rating.count
    return (
-      <Card.Root key={product.id} maxW="sm" h="100%" overflow="hidden">
+      <Card.Root opacity={productInStock > 0 ? 1 : 0.6} key={product.id} maxW="sm" h="100%" overflow="hidden">
          <Image 
             fit="scale-down" 
             h="250px"
@@ -17,6 +18,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
          </Card.Body>
          <Card.Footer gap="4" flexDirection="column" alignItems="start">
             <Text justifySelf="end" textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">{product.price && `${product.price}$`}</Text>
+            <Text fontSize="0.875rem" color="#1560BD">{productInStock > 0 ? `Осталось ${productInStock} шт.` : "Нет в наличии"} </Text>
             <Button variant="solid">Добавить в корзину</Button>
          </Card.Footer>
       </Card.Root>
